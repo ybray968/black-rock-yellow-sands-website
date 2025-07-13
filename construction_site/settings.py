384c3377b2f5d7f916326bbd98e03448226046b4
Al Muaddib <al.muaddib@construction.com>
2025-07-14 00:08:00 +0200

    Switch to Gmail SMTP for reliable email delivery
    - Updated EMAIL_HOST to smtp.gmail.com (default)
    - Set EMAIL_PORT to 587 with TLS enabled
    - More reliable for production deployment on Railway
    - Requires Gmail app password for authentication

--- construction_site/settings.py
+++ construction_site/settings.py
@@ -251,11 +251,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Email configuration for ProtonMail
+# Email configuration for Gmail SMTP
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.protonmail.com')
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
