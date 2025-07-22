f4e06b49f4fdf204e10b6010350d0e2cbea58b69
Al Muaddib <al.muaddib@blackrockyellowsands.com>
2025-07-22 11:13:29 +0200

    Remove pine lumber button, views, and templates

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,6 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('pine-lumber/', views.pine_lumber, name='pine_lumber'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('health/', views.health_check, name='health_check'),
